a7f3ce1a8d4f9595dad67ba2a612fed44050bbe3
Philipp <philipp.wundrack@live.de>
2020-12-15 20:51:05 +0100

    refactor training with pennylane to make it more flexible

--- pennylane_test.py
+++ pennylane_test.py
@@ -4,50 +4,62 @@ from typing import List
 import pennylane as qml
 import torch
 import numpy as np
+from qiskit import QuantumCircuit
 from qiskit.circuit import Parameter
 
 import QNN1
 import QNN2
 import QNN3
 
-dev = qml.device("default.qubit", wires=3, shots=1000, analytic=False)
-qc = QNN2.create_qiskit_circuit("", 3)
-params: List[Parameter] = list(qc.parameters)
+
+def generate_circuit_func(params: List[Parameter], qc: QuantumCircuit):
+	def func(weights: torch.Tensor):
+		value_dict = {}
+
+		for param, value in zip(params, weights):
+			value_dict[param] = value
+
+		qml.from_qiskit(qc)(value_dict)
+
+		return qml.expval(qml.PauliZ(0)), qml.expval(qml.PauliZ(1)), qml.expval(qml.PauliZ(2))
+
+	return func
 
 
-@qml.qnode(dev, interface="torch")
-def circuit(weights: torch.Tensor):
-	value_dict = {}
-
-	for param, value in zip(params, weights):
-		value_dict[param] = value
-
-	qml.from_qiskit(qc)(value_dict)
-
-	return qml.expval(qml.PauliZ(0)), qml.expval(qml.PauliZ(1)), qml.expval(qml.PauliZ(2))
-
-
-def cost(weights: torch.Tensor, target: torch.Tensor):
-	output = (circuit(weights) - 1) / -2.0  # normalizes the expected values
+def cost(weights: torch.Tensor, target: torch.Tensor, qnode: qml.QNode):
+	output = (qnode(weights) - 1) / -2.0  # normalizes the expected values
 
 	return torch.mean((output - target) ** 2)
 
 
-weights = torch.tensor(np.random.rand(len(params)) * 2 * pi, requires_grad=True)
-target = torch.tensor([0.8, 0.8, 0.8], requires_grad=False)
+def train_loop(
+		iterations: int, weights: torch.Tensor, target: torch.Tensor, qnode: qml.QNode):
+	opt = torch.optim.Adam([weights], lr=0.1)
 
-opt = torch.optim.Adam([weights], lr=0.1)
-steps = 200
+	def closure():
+		opt.zero_grad()
+		loss = cost(weights, target, qnode)
+		loss.backward()
+		print(loss.item())
+
+		return loss.item()
+
+	for i in range(iterations):
+		opt.step(closure)
 
 
-def closure():
-	opt.zero_grad()
-	loss = cost(weights, target)
-	loss.backward()
-	print(loss.item())
+def test_training():
+	dev = qml.device("default.qubit", wires=3, shots=1000, analytic=False)
+	qc = QNN1.create_qiskit_circuit("", 3)
+	params: List[Parameter] = list(qc.parameters)
 
-	return loss
+	weights = torch.tensor(np.random.rand(len(params)) * 2 * pi, requires_grad=True)
+	target = torch.tensor([0.8, 0.8, 0.8], requires_grad=False)
+
+	qnode = qml.QNode(generate_circuit_func(params, qc), dev, interface="torch")
+
+	train_loop(100, weights, target, qnode)
 
 
-for i in range(steps):
-	opt.step(closure)
+if __name__ == "__main__":
+	test_training()
