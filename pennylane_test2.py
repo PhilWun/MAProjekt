2f077115e340226360833a4b041122921bbe3bda
Philipp <philipp.wundrack@live.de>
2020-12-21 18:56:58 +0100

    refactor training loop

--- pennylane_test2.py
+++ pennylane_test2.py
@@ -2,6 +2,7 @@ from typing import List
 
 import pennylane as qml
 import pennylane.numpy as np
+from pennylane import QNode
 
 q_num = 3
 
@@ -27,17 +28,7 @@ def qnn3(input_values: np.tensor, weights: np.tensor) -> List[np.tensor]:
 	return [qml.expval(qml.PauliZ(wires=i)) for i in range(q_num)]
 
 
-opt = qml.GradientDescentOptimizer(stepsize=0.1)
-steps = 100
-# params = np.array([0.1, 0.2, 0.3, 0.4, 0.5, 0.6])
-params = np.array(np.random.rand(6))
-input_values = np.array([[0, np.pi, 0], [np.pi, 0, np.pi]], requires_grad=False)
-target = np.array([[-1, 1, -1], [1, -1, 1]], requires_grad=False)
-dev = qml.device('default.qubit', wires=3, shots=1000, analytic=False)
-qnode = qml.QNode(qnn3, dev)
-
-
-def generate_cost_func(input_values: np.tensor, target: np.tensor):
+def generate_cost_func(qnode: QNode, input_values: np.tensor, target: np.tensor):
 	def cost(weights: np.tensor):
 		result = np.mean((qnode(input_values, weights) - target) ** 2)
 		print(result)
@@ -47,11 +38,32 @@ def generate_cost_func(input_values: np.tensor, target: np.tensor):
 	return cost
 
 
-for i in range(steps):
-	for j in range(input_values.shape[0]):
-		cost = generate_cost_func(input_values[j], target[j])
-		params = opt.step(cost, params)
+def training_loop(qnode: QNode, input_values: np.tensor, target: np.tensor, params: np.tensor, optimizer, steps: int):
+	for i in range(steps):
+		for j in range(input_values.shape[0]):
+			cost = generate_cost_func(qnode, input_values[j], target[j])
+			params = optimizer.step(cost, params)
 
-print(params)
-print(qnode(input_values[0], params))
-print(qnode(input_values[1], params))
+	return params
+
+
+def test_qnn3():
+	dev = qml.device('default.qubit', wires=3, shots=1000, analytic=False)
+	qnode = qml.QNode(qnn3, dev)
+
+	params = np.array(np.random.rand(6))
+	input_values = np.array([[0, np.pi, 0]], requires_grad=False)
+	target = np.array([[-1, 1, -1]], requires_grad=False)
+
+	opt = qml.GradientDescentOptimizer(stepsize=0.1)
+	steps = 100
+
+	params = training_loop(qnode, input_values, target, params, opt, steps)
+
+	print(params)
+	print(qnode(input_values[0], params))
+	# print(qnode(input_values[1], params))
+
+
+if __name__ == "__main__":
+	test_qnn3()
