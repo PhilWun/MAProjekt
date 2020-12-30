9805bcb5224afd745a42106ca50a4b4b72e62f0a
Philipp <philipp.wundrack@live.de>
2020-12-30 23:35:56 +0100

    refactor training

--- src/pl/pytorch_examples.py
+++ src/pl/pytorch_examples.py
@@ -1,34 +1,47 @@
 from math import pi
+from typing import Callable, Tuple
 
 import pennylane as qml
 import torch
 import QNN1
 
 
-def example_train_qnn1():
-    q_num = 3
+def create_qlayer(constructor_func: Callable, q_num: int) -> qml.qnn.TorchLayer:
     dev = qml.device('default.qubit', wires=q_num, shots=1000, analytic=False)
     circ_func, param_num = QNN1.constructor(q_num)
     qnode = qml.QNode(circ_func, dev, interface="torch")
+
     weight_shapes = {"weights": param_num}
     qlayer = qml.qnn.TorchLayer(qnode, weight_shapes)
+
+    return qlayer
+
+
+def training_loop(
+        model: torch.nn.Module, inputs: torch.Tensor, target: torch.Tensor, opt: torch.optim.Optimizer, steps: int):
+    loss_func = torch.nn.MSELoss()
+
+    for i in range(steps):
+        opt.zero_grad()
+        loss = loss_func(model(inputs), target)
+        loss.backward()
+        opt.step()
+        print(loss.item())
+
+
+def example_train_qnn1():
+    q_num = 3
+    qlayer = create_qlayer(QNN1.constructor, q_num)
+
     model = torch.nn.Sequential(
         qlayer
     )
 
-    input_data = torch.tensor([0.0] * q_num, requires_grad=False)
+    inputs = torch.tensor([0.0] * q_num, requires_grad=False)
     target = torch.tensor([0.8] * q_num, requires_grad=False)
-
     opt = torch.optim.Adam(model.parameters(), lr=0.1)
-    loss_func = torch.nn.MSELoss()
-    steps = 100
 
-    for i in range(steps):
-        opt.zero_grad()
-        loss = loss_func(model(input_data), target)
-        loss.backward()
-        opt.step()
-        print(loss.item())
+    training_loop(model, inputs, target, opt, 100)
 
 
 if __name__ == "__main__":
