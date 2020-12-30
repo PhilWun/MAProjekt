e5e4501b8d11fefc0adb03e018ee33840ebaf7d2
Philipp <philipp.wundrack@live.de>
2020-12-30 03:02:08 +0100

    add CLI to the Qiskit training

--- src/qk/main.py
+++ src/qk/main.py
@@ -1,5 +1,7 @@
+import argparse
 from typing import List, Callable, Optional
 
+import mlflow
 import numpy as np
 from qiskit import QuantumCircuit, transpile, assemble
 from qiskit.aqua.components.optimizers import Optimizer, ADAM, CG, COBYLA, L_BFGS_B, GSLS, NELDER_MEAD, NFT, POWELL, SLSQP, SPSA, TNC
@@ -7,51 +9,22 @@ from qiskit.providers import BaseBackend
 from qiskit.providers.aer import QasmSimulator, AerJob
 
 import QNN1
-from src.qk.tools import train_circuit, get_expected_values
+import QNN2
+import QNN3
+from tools import train_circuit, get_expected_values
 
 
-def execute_optimizer(
-		transpiled_circuit: QuantumCircuit, backend, target: np.ndarray, param_num: int, optim: Optimizer, num_runs: int,
-		target_error: Optional[float] = None):
-	print(type(optim).__name__)
-	iter_num_list: List[float] = []
-	last_error_list: List[float] = []
-	time_list: List[float] = []
+# TODO: add input to circuit
+def execute_optimizer(transpiled_circuit: QuantumCircuit, backend, target: np.ndarray, param_num: int, optim: Optimizer):
+	eval_num = [0]  # hacky way to store an integer in an object to use it inside the callback function
 
-	for _ in range(num_runs):
-		intermediate_errors = []
+	def error_callback(err: float):
+		print("Function evaluation:", eval_num[0], "MSE: ", err)
+		mlflow.log_metric("Training MSE", err, eval_num[0])
+		eval_num[0] = eval_num[0] + 1
 
-		def error_callback(err: float):
-			intermediate_errors.append(err)
-
-		obj_func = Optimizer.wrap_function(mean_squared_error, (transpiled_circuit, backend, target, error_callback))
-		error, train_time, param_values = train_circuit(param_num, obj_func, optim)
-		last_error_list.append(error)
-		time_list.append(train_time)
-		print("objective function calls:", len(intermediate_errors))
-
-		if target_error is not None:
-			converged = False
-
-			for i, err in enumerate(intermediate_errors):
-				if err < 0.01:
-					iter_num_list.append(i + 1)
-					converged = True
-					break
-
-			if not converged:
-				print("not converged")
-
-	print()
-
-	if target_error is not None:
-		print("number of executed circuits till the target error was reached")
-		print("mean:", np.mean(iter_num_list))
-		print("standard deviation:", np.std(iter_num_list))
-		print()
-
-	print("error (mean):", np.mean(last_error_list))
-	print("error (std):", np.std(last_error_list))
+	obj_func = Optimizer.wrap_function(mean_squared_error, (transpiled_circuit, backend, target, error_callback))
+	error, train_time, param_values = train_circuit(param_num, obj_func, optim)
 
 
 def _bind_params(circ: QuantumCircuit, values: np.ndarray):
@@ -106,7 +79,7 @@ def test_all_optimizers():
 	transpiled_qnn: QuantumCircuit = transpile(qnn, backend=backend)
 
 	for optim in optimizers:
-		execute_optimizer(transpiled_qnn, backend, np.array([0.8, 0.8, 0.8]), len(transpiled_qnn.parameters), optim, 10, target_error=0.01)
+		execute_optimizer(transpiled_qnn, backend, np.array([0.8, 0.8, 0.8]), len(transpiled_qnn.parameters), optim)
 
 
 def test_training():
@@ -122,6 +95,123 @@ def test_training():
 	print(measure_output(param_values, transpiled_qnn, backend))
 
 
+def cli():
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--qnn", type=str)
+	parser.add_argument("--qnum", type=int)
+	parser.add_argument("--optimizer", type=str)
+	parser.add_argument("--maxiter", type=int)
+	parser.add_argument("--tol", type=float)
+	parser.add_argument("--lr", type=float)
+	parser.add_argument("--beta1", type=float)
+	parser.add_argument("--beta2", type=float)
+	parser.add_argument("--noise_factor", type=float)
+	parser.add_argument("--eps", type=float)  # alias: epsilon
+	parser.add_argument("--gtol", type=float)  # alias: min_gradient_norm
+	parser.add_argument("--rhobeg", type=float)
+	parser.add_argument("--maxfun", type=int)  # alias: max_eval, maxfev
+	parser.add_argument("--factr", type=float)
+	parser.add_argument("--sampling_radius", type=float)
+	parser.add_argument("--sampling_size_factor", type=int)
+	parser.add_argument("--initial_step_size", type=float)
+	parser.add_argument("--min_step_size", type=float)
+	parser.add_argument("--step_size_multiplier", type=float)
+	parser.add_argument("--armijo_parameter", type=float)
+	parser.add_argument("--max_failed_rejection_sampling", type=int)
+	parser.add_argument("--xtol", type=float)  # alias: xatol
+	parser.add_argument("--adaptive", type=int)  # 0: false, 1: true
+	parser.add_argument("--reset_interval", type=int)
+	parser.add_argument("--ftol", type=float)
+	parser.add_argument("--c0", type=float)
+	parser.add_argument("--c1", type=float)
+	parser.add_argument("--c2", type=float)
+	parser.add_argument("--c3", type=float)
+	parser.add_argument("--c4", type=float)
+	parser.add_argument("--skip_calibration", type=int)  # 0: false, 1: true
+	parser.add_argument("--accuracy", type=float)
+
+	args = parser.parse_args()
+
+	qnn_name = args.qnn
+	qnum = args.qnum
+	optimizer_name = args.optimizer
+	maxiter = args.maxiter
+	tol = args.tol
+	lr = args.lr
+	beta1 = args.beta1
+	beta2 = args.beta2
+	noise_factor = args.noise_factor
+	eps = args.eps
+	gtol = args.gtol
+	rhobeg = args.rhobeg
+	maxfun = args.maxfun
+	factr = args.factr
+	sampling_radius = args.sampling_radius
+	sampling_size_factor = args.sampling_size_factor
+	initial_step_size = args.initial_step_size
+	min_step_size = args.min_step_size
+	step_size_multiplier = args.step_size_multiplier
+	armijo_parameter = args.armijo_parameter
+	max_failed_rejection_sampling = args.max_failed_rejection_sampling
+	xtol = args.xtol
+	adaptive = args.adaptive
+	reset_interval = args.reset_interval
+	ftol = args.ftol
+	c0 = args.c0
+	c1 = args.c1
+	c2 = args.c2
+	c3 = args.c3
+	c4 = args.c4
+	skip_calibration = args.skip_calibration
+	accuracy = args.accuracy
+
+	optimizer = None
+
+	if optimizer_name == "ADAM":
+		optimizer = ADAM(maxiter, tol, lr, beta1, beta2, noise_factor, eps, False)
+	elif optimizer_name == "AMSGRAD":
+		optimizer = ADAM(maxiter, tol, lr, beta1, beta2, noise_factor, eps, True)
+	elif optimizer_name == "CG":
+		optimizer = CG(maxiter, False, gtol, tol, eps)
+	elif optimizer_name == "COBYLA":
+		optimizer = COBYLA(maxiter, False, rhobeg, tol)
+	elif optimizer_name == "L_BFGS_B":
+		optimizer = L_BFGS_B(maxfun, maxiter, factr, -1, eps)
+	elif optimizer_name == "GSLS":
+		optimizer = GSLS(maxiter, maxfun, False, sampling_radius, sampling_size_factor, initial_step_size, min_step_size, step_size_multiplier, armijo_parameter, gtol, max_failed_rejection_sampling)
+	elif optimizer_name == "NELDER_MEAD":
+		optimizer = NELDER_MEAD(maxiter, maxfun, False, xtol, tol, bool(adaptive))
+	elif optimizer_name == "NFT":
+		optimizer = NFT(maxiter, maxfun, False, reset_interval)
+	elif optimizer_name == "POWELL":
+		optimizer = POWELL(maxiter, maxfun, False, xtol, tol)
+	elif optimizer_name == "SLSQP":
+		optimizer = SLSQP(maxiter, False, ftol, tol, eps)
+	elif optimizer_name == "SPSA":
+		optimizer = SPSA(maxiter, 1, 1, c0, c1, c2, c3, c4, bool(skip_calibration))
+	elif optimizer_name == "TNC":
+		optimizer = TNC(maxiter, False, accuracy, ftol, xtol, gtol, tol, eps)
+	else:
+		raise ValueError()
+
+	qnn: QuantumCircuit
+
+	if qnn_name == "QNN1":
+		qnn = QNN1.create_qiskit_circuit("", qnum)
+	elif qnn_name == "QNN2":
+		qnn = QNN2.create_qiskit_circuit("", qnum)
+	elif qnn_name == "QNN3":
+		qnn = QNN3.create_qiskit_circuit("", qnum)
+	else:
+		raise ValueError()
+
+	backend = QasmSimulator()
+	transpiled_qnn: QuantumCircuit = transpile(qnn, backend=backend)
+
+	execute_optimizer(transpiled_qnn, backend, np.array([0.8] * qnum), len(transpiled_qnn.parameters), optimizer)
+
+
 if __name__ == "__main__":
-	test_all_optimizers()
+	# test_all_optimizers()
 	# test_training()
+	cli()
