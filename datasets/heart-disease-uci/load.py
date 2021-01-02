0189f26adf21cc637fbd7301fb48bc9d790117ef
Philipp <philipp.wundrack@live.de>
2021-01-02 18:59:01 +0100

    refactor data loading

--- datasets/heart-disease-uci/load.py
+++ datasets/heart-disease-uci/load.py
@@ -5,11 +5,7 @@ import pandas
 import sklearn.preprocessing as preprocessing
 from sklearn.model_selection import train_test_split
 
-
-class ColumnType(Enum):
-	NUMERIC = 0
-	CATEGORICAL = 1
-	LABEL = 2
+from datasets.tools import ColumnType, load_csv
 
 
 def load_dataset(test_set_fraction: float = 0.3, rnd_seed: float = 0):
@@ -35,29 +31,9 @@ def load_dataset(test_set_fraction: float = 0.3, rnd_seed: float = 0):
 		ColumnType.LABEL
 	]
 
-	dataframe = pandas.read_csv("heart.csv")
-	np_data = dataframe.to_numpy()
-	preprocessed_columns = []
-	labels = None
-
-	for i, ct in enumerate(column_types):
-		column_data = np_data[:, i].reshape((-1, 1))
-
-		if ct == ColumnType.NUMERIC:
-			scaler = preprocessing.MinMaxScaler()
-			preprocessed_columns.append(scaler.fit_transform(column_data))
-		elif ct == ColumnType.CATEGORICAL:
-			enc = preprocessing.OneHotEncoder(sparse=False)
-			preprocessed_columns.append(enc.fit_transform(column_data))
-		elif ct == ColumnType.LABEL:
-			enc = preprocessing.OrdinalEncoder()
-			labels = enc.fit_transform(column_data).reshape((-1, ))
-		else:
-			raise ValueError()
-
-	preprocessed_array = np.concatenate(preprocessed_columns, axis=1)
+	data, labels = load_csv("heart.csv", column_types)
 	input_train, input_test, target_train, target_test = train_test_split(
-		preprocessed_array, labels, test_size=test_set_fraction, random_state=rnd_seed)
+		data, labels, test_size=test_set_fraction, random_state=rnd_seed)
 
 	return input_train, target_train, input_test, target_test
 
---
+++ datasets/tools.py
@@ -0,0 +1,38 @@
+from enum import Enum
+from typing import List, Tuple
+
+import numpy as np
+import pandas as pd
+from sklearn import preprocessing
+
+
+class ColumnType(Enum):
+	NUMERIC = 0
+	CATEGORICAL = 1
+	LABEL = 2
+
+
+def load_csv(file: str, column_types: List[ColumnType]) -> Tuple[np.ndarray, np.ndarray]:
+	dataframe = pd.read_csv(file)
+	np_data = dataframe.to_numpy()
+	preprocessed_columns = []
+	labels = None
+
+	for i, ct in enumerate(column_types):
+		column_data = np_data[:, i].reshape((-1, 1))
+
+		if ct == ColumnType.NUMERIC:
+			scaler = preprocessing.MinMaxScaler()
+			preprocessed_columns.append(scaler.fit_transform(column_data))
+		elif ct == ColumnType.CATEGORICAL:
+			enc = preprocessing.OneHotEncoder(sparse=False)
+			preprocessed_columns.append(enc.fit_transform(column_data))
+		elif ct == ColumnType.LABEL:
+			enc = preprocessing.OrdinalEncoder()
+			labels = enc.fit_transform(column_data).reshape((-1,))
+		else:
+			raise ValueError()
+
+	preprocessed_array = np.concatenate(preprocessed_columns, axis=1)
+
+	return preprocessed_array, labels
