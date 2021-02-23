c0c103be1be7d23b389c338bb8a3c7314af674f9
Philipp <philipp.wundrack@live.de>
2021-02-23 20:51:30 +0100

    add clustering performance metrics

--- src/clustering/cli.py
+++ src/clustering/cli.py
@@ -6,6 +6,7 @@ import mlflow
 from sklearn.cluster import AffinityPropagation, AgglomerativeClustering, DBSCAN, FeatureAgglomeration, OPTICS, \
 	SpectralClustering, Birch, KMeans, MiniBatchKMeans, MeanShift, SpectralBiclustering, SpectralCoclustering
 import numpy as np
+from sklearn import metrics
 
 sys.path.append(".")
 
@@ -17,14 +18,12 @@ if __name__ == "__main__":
 	parser.add_argument("--run_id", type=str)
 	parser.add_argument("--cluster_algo", type=str)
 	parser.add_argument("--cluster_args", type=str)
-	parser.add_argument("--step", type=int)
 
 	args = parser.parse_args()
 
 	run_id: str = args.run_id
 	cluster_algo_name: str = args.cluster_algo
 	cluster_args: str = args.cluster_args
-	step: int = args.step
 
 	if cluster_algo_name == "AffinityPropagation":
 		args = parse_arg_str(
@@ -116,29 +115,33 @@ if __name__ == "__main__":
 	embeddings_folder_path = os.path.join(artifact_path, "embeddings")
 	labels_folder_path = os.path.join(artifact_path, "labels")
 
-	if step == -1:
-		embeddings_path = os.path.join(embeddings_folder_path, sorted(os.listdir(embeddings_folder_path))[-1])
-		labels_path = os.path.join(labels_folder_path, sorted(os.listdir(labels_folder_path))[-1])
-	else:
-		embeddings_path = os.path.join(embeddings_folder_path, "{:06}_embeddings.csv".format(step))
-		labels_path = os.path.join(labels_folder_path, "{:06}_labels.csv".format(step))
+	embeddings_files = sorted(os.listdir(embeddings_folder_path))
+	lables_files = sorted(os.listdir(labels_folder_path))
 
-	print(embeddings_path)
-	print(labels_path)
-	embeddings = np.loadtxt(embeddings_path, delimiter=",")
-	real_labels = np.loadtxt(labels_path, delimiter=",")
+	for i, (embedding_file, lable_file) in enumerate(zip(embeddings_files, lables_files)):
+		embeddings = np.loadtxt(os.path.join(embeddings_folder_path, embedding_file), delimiter=",")
+		real_labels = np.loadtxt(os.path.join(labels_folder_path, lable_file), delimiter=",")
 
-	cluster_labels = cluster_algo.fit_predict(embeddings)
-	clusters = {}
+		cluster_labels = cluster_algo.fit_predict(embeddings)
+		clusters = {}
 
-	for cluster_label in np.unique(cluster_labels):
-		labels_in_cluster = np.compress(cluster_labels == cluster_label, real_labels).astype(dtype=np.int)
-		label_counts = np.bincount(labels_in_cluster)
-		label_dict = {}
+		for cluster_label in np.unique(cluster_labels):
+			labels_in_cluster = np.compress(cluster_labels == cluster_label, real_labels).astype(dtype=np.int)
+			label_counts = np.bincount(labels_in_cluster)
+			label_dict = {}
 
-		for i, count in enumerate(label_counts):
-			label_dict["Label " + str(i)] = int(count)
+			for j, count in enumerate(label_counts):
+				label_dict["Label " + str(j)] = int(count)
 
-		clusters["Cluster " + str(cluster_label)] = label_dict
+			clusters["Cluster " + str(cluster_label)] = label_dict
 
-	mlflow.log_dict(clusters, "clusters.json")
+		mlflow.log_dict(clusters, "clusters/{:06}.json".format(i))
+
+		mlflow.log_metric("rand_index", metrics.rand_score(real_labels, cluster_labels), i)
+		mlflow.log_metric("mutual_info_score", metrics.mutual_info_score(real_labels, cluster_labels), i)
+		mlflow.log_metric("adjusted_mutual_info_score", metrics.adjusted_mutual_info_score(real_labels, cluster_labels), i)
+		mlflow.log_metric("normalized_mutual_info_score", metrics.normalized_mutual_info_score(real_labels, cluster_labels), i)
+		mlflow.log_metric("homogeneity_score", metrics.homogeneity_score(real_labels, cluster_labels), i)
+		mlflow.log_metric("completeness_score", metrics.completeness_score(real_labels, cluster_labels), i)
+		mlflow.log_metric("v_measure_score", metrics.v_measure_score(real_labels, cluster_labels), i)
+		mlflow.log_metric("fowlkes_mallows_score", metrics.fowlkes_mallows_score(real_labels, cluster_labels), i)
