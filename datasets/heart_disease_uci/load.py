f1f146cc872268fe049af1f03bb0890c35320058
Philipp <philipp.wundrack@live.de>
2021-03-05 19:45:57 +0100

    enable shuffle for the train-test split

--- datasets/heart_disease_uci/load.py
+++ datasets/heart_disease_uci/load.py
@@ -28,7 +28,7 @@ def load_dataset(test_set_fraction: float = 0.3, rnd_seed: int = 0):
 
 	data, labels = load_csv("/home/philipp/MEGA/Studium/PlanQK/MA/Projekt/datasets/heart_disease_uci/heart.csv", column_types)
 	input_train, input_test, target_train, target_test = train_test_split(
-		data, labels, test_size=test_set_fraction, random_state=rnd_seed, shuffle=False)
+		data, labels, test_size=test_set_fraction, random_state=rnd_seed, shuffle=True)
 
 	return input_train, target_train, input_test, target_test
 
